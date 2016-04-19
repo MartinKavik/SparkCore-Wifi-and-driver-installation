7aca7baecd712473860da89a79520581092797d6
Martin Kavík <martin.kavik@abrigo.cz>
2016-04-19 21:35:52 +0200

    content

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-SparkCore-Wifi-and-driver-installation
+# SparkCore-Wifi-and-driver-installation
+Example of C# GUI application for Spark Core wifi settings and driver installation
+
+Tested on Windows 7 x64
+
+![alt tag](screen.png)
---
+++ SparkCore_Init/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SparkCore_Init
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.Title = "Spark Core - basic settings";
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }    
+}
